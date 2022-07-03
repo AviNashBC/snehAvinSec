a44fe9480043c5a1cf9d5cda9219e453a986795f
AviNashBC <107311681+AviNashBC@users.noreply.github.com>
2022-07-03 13:51:45 +0200

    First Commit as Mgr

---
+++ src/test/java/snehAviSecPrj_01/openGoogle02.java
@@ -0,0 +1,19 @@
+package snehAviSecPrj_01;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class openGoogle02 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		Thread.sleep(5000);
+		driver.close();	
+		}
+
+}
